3815417677fb2928cb1594a3f217e4d9bcb372cc
mangowhoiscloud <ryoo0504@gmail.com>
2025-12-17 19:27:53 +0900

    fix(proto): grpc 파일의 절대 import를 상대 import로 수정
    grpcio-tools가 생성한 _pb2_grpc.py 파일들이 절대 import를 사용하여
    패키지 구조 내에서 ModuleNotFoundError 발생
    
    - character_pb2_grpc.py: import character_pb2 → from . import character_pb2
    - user_character_pb2_grpc.py: import user_character_pb2 → from . import user_character_pb2

--- domains/character/proto/my/user_character_pb2_grpc.py
+++ domains/character/proto/my/user_character_pb2_grpc.py
@@ -2,7 +2,7 @@
 """Client and server classes corresponding to protobuf-defined services."""
 import grpc
 
-import user_character_pb2 as user__character__pb2
+from . import user_character_pb2 as user__character__pb2
 
 GRPC_GENERATED_VERSION = "1.68.1"
 GRPC_VERSION = grpc.__version__
