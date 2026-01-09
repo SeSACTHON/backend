e2d30077530e78b69c9115b12f11b0ce11cd2869
mangowhoiscloud <ryoo0504@gmail.com>
2026-01-09 17:01:49 +0900

    fix(workers): reward_character_task autodiscover를 위해 __init__.py에 import 추가

--- apps/users_worker/presentation/tasks/__init__.py
+++ apps/users_worker/presentation/tasks/__init__.py
@@ -1,5 +1,6 @@
 """Celery Tasks."""
 
 from users_worker.presentation.tasks.character_task import save_characters_task
+from users_worker.presentation.tasks.reward_event_task import reward_character_task
 
-__all__ = ["save_characters_task"]
+__all__ = ["save_characters_task", "reward_character_task"]
